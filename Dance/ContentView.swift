7eb758a91f4bf9cfe81e45a788727a6799aa58f1
Cih <angel08012000@gmail.com>
2020-11-22 12:12:32 +0800

    Initial Commit

---
+++ Dance/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dance
+//
+//  Created by 慈慈 on 2020/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dance/DanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DanceApp.swift
+//  Dance
+//
+//  Created by 慈慈 on 2020/11/22.
+//
+
+import SwiftUI
+
+@main
+struct DanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
